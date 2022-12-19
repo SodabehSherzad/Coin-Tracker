7c0936c6a8a1f2eb6e3a59538f2644d55f5ec41a
Sodabeh Sherzad <sodabehsherzad2018@gmail.com>
2022-12-19 17:46:34 +0100

    Do all Todos

---
+++ lib/constants.dart
@@ -0,0 +1,3 @@
+//TODO: 1.3.3 Put your API key and CoinAPI's base url in the following constants
+const kAPIKey = '8978535B-557B-41A5-9025-F23AD07D91B0';
+const kBaseURL = 'https://rest.coinapi.io/v1/exchangerate';
